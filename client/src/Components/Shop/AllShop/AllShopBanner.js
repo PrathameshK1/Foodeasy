44ead9c7f599308874b5e33fb60b033a65c1476c
Pradnyesh2711 <100141334+Pradnyesh2711@users.noreply.github.com>
2023-04-30 00:11:14 +0530

    xy

--- client/src/Components/Shop/AllShop/AllShopBanner.js
+++ client/src/Components/Shop/AllShop/AllShopBanner.js
@@ -4,7 +4,7 @@ import vendorData from '../../../Data/AllShopData';
 const AllShopBanner = () => {
     
     return (  
-        <div class="w-full bg-center bg-cover h-56" style={{backgroundImage: `url(https://blog.ipleaders.in/wp-content/uploads/2017/11/BV-Acharya-48.jpg)`}}>
+        <div class="w-full bg-center bg-cover h-56" style={{backgroundImage:`url(https://blog.ipleaders.in/wp-content/uploads/2017/11/BV-Acharya-48.jpg)`}}>
             <div class="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-60">
                 <div class="text-center">
                     <h1 class="text-4xl font-semibold text-white uppercase lg:text-4xl">All Registered Shops</h1>
